9ef8cc9dde3a0a6524eab81587f7c148ec41ad2d
dannyirumva <boyg87059@gmail.com>
2024-01-30 10:04:22 +0200

    HelloWorldImplementation

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloWorldController extends AbstractController
+{
+    #[Route('/hello/world', name: 'app_hello_world')]
+    public function index():Response
+    {
+    $data = [
+        'name' => 'John Doe',
+        'message' => 'Hello from Symfony!',
+    ];
+
+    return $this->render('my_template.html.twig', $data);
+    }
+}
